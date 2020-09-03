f89d72308e4124248740303287f2b72abd5d625e
yuvalotem <yuvalotem@gmail.com>
2020-09-03 12:19:47 +0300

    finished

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -1,15 +1,15 @@
 class Render {
     constructor() { }
 
-    render(data) {
+    renderer(data) {
         if (data[0] != undefined) {
             const source = $('#recipes-template').html()
             const template = Handlebars.compile(source)
             const newHTML = template({ data })
             $('#recipes-container').empty().append(newHTML)
         } else {
-            const newHTML = "<div class='error'> Recipes is unavailabe</div>"
-            $('#players').append(newHTML)
+            const newHTML = "<div id='error'>No recipes for this ingredient</div>"
+            $('#recipes-container').append(newHTML)
         }
     }
 }
--- dist/main.js
+++ dist/main.js
@@ -2,6 +2,12 @@ const display = new Render()
 $('#button').on('click', function () {
     const input = $('#input').val()
     $.get(`/recipes/${input}`, function (data) {
-        display.render(data)
+        display.renderer(data)
+        $('#input').val('')
     })
+})
+
+$('#recipes-container').on('click', '.dish-img', function () {
+   const firstIngred = $(this).closest('div').find( "li" ).first().text()
+   alert(firstIngred)
 })
